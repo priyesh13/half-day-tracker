93e20425c061ca6b9880da71b03bb669f0934e48
priyesh13 <mistrypriyesh13@gmail.com>
2015-04-23 14:32:33 +0100

    Tidied add person form

--- client/app/person/add-a-person/add-person-inline.directive.js
+++ client/app/person/add-a-person/add-person-inline.directive.js
@@ -1,18 +1,17 @@
 'use strict';
 
 angular.module('halfDayTrackerApp')
-  .directive('addPersonInline', function ($compile) {
+  .directive('addPersonInline', function ($compile, $templateRequest) {
     return {
       controller: 'AddPersonCtrl',
       link: function (scope, element, attrs) {
         element.bind('click', function() {
           scope.isFormOpen = true;
-          angular.element(document.getElementById('space-for-new-person'))
-            .append($compile('<form novalidate name="add-person-form" id="add-person-form" class="add-person-form"><input type="text" data-ng-model="person.name" placeholder="Name"><input type="text" data-ng-model="person.initials" placeholder="Initials"><button type="button" class="btn btn-default" data-ng-click="addPerson()">Submit</button><button type="button" class="btn" data-ng-click="cancelForm()">Cancel</button></form>')(scope));
+          $templateRequest("app/person/add-a-person/add-a-person-form.html").then(function(html) {
+            console.log($compile(html))
+            angular.element(document.getElementById('space-for-new-person')).append($compile(html)(scope));
+          });
         })
       }
     };
   });
-
-
-
