cc8765fc3105d6fe9e9933332984519135cd3a46
telly <telly@bskyb.com>
2015-05-08 15:20:40 +0100

    enabling datepicker on datepicker branch

--- client/app/person/person.controller.js
+++ client/app/person/person.controller.js
@@ -4,9 +4,9 @@ angular.module('halfDayTrackerApp')
   .controller('PersonCtrl', function ($scope, $http) {
 
     $scope.useHalfDay = function($event) {
-      //$scope.open($event)
-      $scope.person.supportHalfDays > 0 ? $scope.person.supportHalfDays -= 1 : $scope.person.bonusHalfDays -= 1;
-      $http.put('/api/people/'+ $scope.person._id, $scope.person);
+      $scope.open($event)
+      //$scope.person.supportHalfDays > 0 ? $scope.person.supportHalfDays -= 1 : $scope.person.bonusHalfDays -= 1;
+      //$http.put('/api/people/'+ $scope.person._id, $scope.person);
     };
 
     $scope.addHalfDay = function() {
